d47f4f9809929d843ef5c410351ce207f4cb7b56
rvanasa <ryan.vandersmith@dfinity.org>
2023-06-07 12:18:52 -0600

    Initial commit

---
+++ backend/src/lib.rs
@@ -0,0 +1,20 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static COUNTER: RefCell<u32> = RefCell::default();
+}
+
+#[ic_cdk_macros::query]
+fn get() -> u32 {
+    COUNTER.with(|counter| *counter.borrow())
+}
+
+#[ic_cdk_macros::update]
+fn inc() {
+    COUNTER.with(|counter| *counter.borrow_mut() += 1)
+}
+
+#[ic_cdk_macros::update]
+fn set(value: u32){
+    COUNTER.with(|counter| *counter.borrow_mut() = value)
+}
