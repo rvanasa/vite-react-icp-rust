b79f25020fb38329c4a0b49fc2a7531b9d8d9977
rvanasa <ryan.vandersmith@dfinity.org>
2023-06-07 14:55:14 -0600

    Use stable memory for counter variable

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,20 +1,67 @@
 use std::cell::RefCell;
 
+use candid::{CandidType, Deserialize};
+use ic_cdk_macros::{init, post_upgrade, pre_upgrade, query, update};
+use ic_stable_memory::{
+    derive::{CandidAsDynSizeBytes, StableType},
+    retrieve_custom_data, stable_memory_init, stable_memory_post_upgrade,
+    stable_memory_pre_upgrade, store_custom_data, SBox,
+};
+
+#[derive(CandidType, Deserialize, StableType, CandidAsDynSizeBytes, Debug, Clone)]
+struct State {
+    counter: u32,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        State { counter: 0 }
+    }
+}
+
 thread_local! {
-    static COUNTER: RefCell<u32> = RefCell::default();
+    static STATE: RefCell<Option<State>> = RefCell::default();
 }
 
-#[ic_cdk_macros::query]
+#[init]
+fn init() {
+    stable_memory_init();
+
+    STATE.with(|s| {
+        *s.borrow_mut() = Some(State::default());
+    });
+}
+
+#[pre_upgrade]
+fn pre_upgrade() {
+    let state = STATE.with(|s| s.borrow_mut().take().unwrap());
+    let boxed_state = SBox::new(state).expect("Out of memory");
+
+    store_custom_data::<State>(0, boxed_state);
+    stable_memory_pre_upgrade().expect("Out of memory");
+}
+
+#[post_upgrade]
+fn post_upgrade() {
+    stable_memory_post_upgrade();
+
+    let state = retrieve_custom_data::<State>(0).unwrap().into_inner();
+    STATE.with(|s| {
+        *s.borrow_mut() = Some(state);
+    });
+}
+
+#[query]
 fn get() -> u32 {
-    COUNTER.with(|counter| *counter.borrow())
+    STATE.with(|s| s.borrow().as_ref().unwrap().counter)
 }
 
-#[ic_cdk_macros::update]
+#[update]
 fn inc() {
-    COUNTER.with(|counter| *counter.borrow_mut() += 1)
+    STATE.with(|s| s.borrow_mut().as_mut().unwrap().counter += 1)
 }
 
-#[ic_cdk_macros::update]
+#[update]
 fn set(value: u32) {
-    COUNTER.with(|counter| *counter.borrow_mut() = value)
+    STATE.with(|s| s.borrow_mut().as_mut().unwrap().counter = value)
 }
